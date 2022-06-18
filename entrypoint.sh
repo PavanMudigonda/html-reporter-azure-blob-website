dfd0d519f18ed4dfbcbbb29c756f29a5f9858bc7
Pavan Mudigonda <mnpawan@gmail.com>
2022-06-18 15:05:11 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,21 +31,6 @@ cat > index-template.html <<EOF
 
 EOF
 
- 
-# Install Azure Copy AzCopy
-
-#Download AzCopy
-wget https://aka.ms/downloadazcopy-v10-linux
-
-#Expand Archive
-tar -xvf downloadazcopy-v10-linux
-
-#(Optional) Remove existing AzCopy version
-sudo rm /usr/bin/azcopy
-
-#Move AzCopy to the destination you want to store it
-sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
-
 #----------------------
 
 mkdir -p ./${INPUT_RESULTS_HISTORY}
