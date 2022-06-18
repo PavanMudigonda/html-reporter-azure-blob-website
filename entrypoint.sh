beeb0dffda82c633f60c2100ebce9e05489be04e
Pavan Mudigonda <mnpawan@gmail.com>
2022-06-18 18:33:58 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -92,7 +92,7 @@ azcopy --version
  
 # Azure Blob Upload
 
-azcopy sync "${INPUT_RESULTS_HISTORY}" "https://${INPUT_ACCOUNT_NAME}/.blob.core.windows.net/${INPUT_CONTAINER}?${INPUT_SAS}" --recursive
+`azcopy sync "${INPUT_RESULTS_HISTORY}" "https://${INPUT_ACCOUNT_NAME}/.blob.core.windows.net/${INPUT_CONTAINER}?${INPUT_SAS}" --recursive`
 
 
 # # Delete history
