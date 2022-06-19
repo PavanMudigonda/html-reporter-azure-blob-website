cf0a540926b28ff185db88d1debe17a8dd274ff5
Pavan Mudigonda <mnpawan@gmail.com>
2022-06-19 07:26:52 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,9 +50,8 @@ cat index-template.html > ./${INPUT_RESULTS_HISTORY}/index.html
 echo "├── <a href="./${INPUT_GITHUB_RUN_NUM}/index.html">Latest Test Results - RUN ID: ${INPUT_GITHUB_RUN_NUM}</a><br>" >> ./${INPUT_RESULTS_HISTORY}/index.html;
 sh -c "azcopy list 'https://${INPUT_ACCOUNT_NAME}.blob.core.windows.net/${INPUT_CONTAINER}?${INPUT_SAS}'" | grep "INFO: " | sed 's/INFO: //' | sort -n | while read line; 
 	do 
-# 	  VAR="$(echo $line | ";
-	  VAR1=89
-          echo "├── <a href="./"${VAR1}"/">RUN ID: "${VAR1}"</a><br>" >> ./${INPUT_RESULTS_HISTORY}/index.html; 
+	  VAR="$(awk -F '/' '{print $1;}' )"; echo $VAR;
+	  echo "├── <a href="./"${VAR}"/">RUN ID: "${VAR}"</a><br>" >> ./${INPUT_RESULTS_HISTORY}/index.html; 
 	done;
 
 echo "</html>" >> ./${INPUT_RESULTS_HISTORY}/index.html;
